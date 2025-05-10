823b9c12e46c08d4231cbc98ab2d235fb2831e46
mozart953 <wilsonnianopop@gmail.com>
2025-05-10 00:26:36 -0600

    implement cookie-based JWT storage in login response; update protected route to validate token from cookies

--- autenticacion/index.js
+++ autenticacion/index.js
@@ -1,5 +1,6 @@
 import express from 'express'
 import jwt from 'jsonwebtoken'
+import cookieParser from 'cookie-parser'
 import { PORT } from './config.js'
 import { UserRepository } from './user-repository.js'
 import { JWT_SECRET } from './config.js'
@@ -8,16 +9,13 @@ const app = express()
 app.set('view engine', 'ejs')
 app.use(express.static('public'));
 app.use(express.json())
-
+app.use(cookieParser())
 
 
 app.get('/', (req, res) => {
     res.render('login')
 })
 
-app.get('/protected', (req, res) => {
-    res.render('protected', {username: req.session.username})
-})
 
 app.post('/login', async (req, res)=>{
     const {username, password} = req.body
@@ -25,7 +23,14 @@ app.post('/login', async (req, res)=>{
     try{
         const user = await UserRepository.login({username, password})
         const token = jwt.sign({id: user._id, username: user.username}, JWT_SECRET,{expiresIn: '1h'})
-        res.send({user, token}) 
+        res.cookie('my_cookie', token, 
+        {
+            httpOnly: true,
+            secure: process.env.NODE_ENV === 'production',
+            sameSite: 'strict',
+            maxAge: 3600000 // 1 hour in milliseconds
+        }
+        ).send({user, token}) 
     }catch(error){
         res.status(400).send(error.message)
     }
@@ -47,7 +52,14 @@ app.post('/register', async (req, res) =>{
 })
 
 app.post('/logout', (req, res) =>{})
-app.post('/protected', (req, res) =>{})
+
+app.get('/protected', (req, res) => {
+    const token = req.cookies.my_token
+    if(!token){
+        return res.status(403).send('Access not authorized')
+    }
+    res.render('protected')
+})
 
 app.listen(PORT, () => {
     console.log(`Servidor corriendo en el puerto ${PORT}`)
